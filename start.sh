272fc26e3932f77211f340dfdfb95576ade71cd9
Linocent <timothee.linocent@gmail.com>
2022-11-08 11:04:16 +0100

    Try to launch docker compose in a screen

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-echo check si le screen existe
-if ! screen -list | grep -q "schtroumpfette"; then
-  screen -S schtroumpfette
-  echo screen créé
-fi
-echo lance le bot
-docker compose up
+screen -dmS schtroumpfette docker compose up
 echo bot lancé
