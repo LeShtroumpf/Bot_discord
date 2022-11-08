4c595c9cc989dd4fe4e264693c01fd33b70a3a9f
Linocent <timothee.linocent@gmail.com>
2022-11-08 09:13:31 +0100

    try to use a screen for launch app.

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker compose up
+echo check si le screen existe
+if ! screen -list | grep -q "schtroumpfette"; then
+  echo lance le bot
+    docker compose up
+fi
+  screen -s schtroumpfette
+  echo screen créé
+  echo lance le bot
+  docker compose up
+echo bot lancé
