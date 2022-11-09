8d15ffd46606ced90a56f94a07418da47039581c
Linocent <timothee.linocent@gmail.com>
2022-11-09 11:00:44 +0100

    remove ip from youtube.py

--- schtroumpfette/commands/youtube.py
+++ schtroumpfette/commands/youtube.py
@@ -20,7 +20,7 @@ ytdl_format_options = {
     'quiet': False,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '78.229.135.231' # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
 ffmpeg_options = {
